f44d0c8c834bc3d9ab0d94163e05ffc590068bd6
skt0725 <skt0725@kaist.ac.kr>
2022-07-06 17:45:20 +0900

    training with epoch 300, lr 0.0002

--- gan.py
+++ gan.py
@@ -1,3 +1,4 @@
+from dis import dis
 import os
 import torch
 import torch.nn as nn
@@ -5,6 +6,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+from torchvision.utils import save_image
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]= "0, 1"
@@ -16,21 +18,27 @@ print('Count of using GPUs:', torch.cuda.device_count())
 print('Current cuda device:', torch.cuda.current_device())
 
 transform = transforms.Compose([
+    transforms.Resize(28),
     transforms.ToTensor(),
     transforms.Normalize([0.5], [1])
 ])
+
+latent_size = 64
+batch_size = 128
+total_epoch = 300
+
 data_root = './data'
 train_data = datasets.MNIST(data_root, transform=transform, train=True, download=True)
 
-dataloader = DataLoader(train_data, batch_size = 128, shuffle=True, num_workers = 4)
+dataloader = DataLoader(train_data, batch_size = batch_size, shuffle=True, num_workers = 4)
 
 # example data
 train_x, train_y = next(iter(dataloader))
 img = train_x[0].squeeze()
-label = train_y[0]
-plt.imshow(img, cmap="gray")
-plt.title = label
-plt.show()
+# label = train_y[0]
+# plt.imshow(img, cmap="gray")
+# plt.title = label
+# plt.show()
 
 # https://machinelearningmastery.com/how-to-train-stable-generative-adversarial-networks/
 # Leaky ReLU for discriminator, relu&tanh for generator
@@ -68,21 +76,55 @@ class Discriminator(nn.Module):
     def forward(self, x):
         output = self.layer(x)
         return output
-latent_size = 64
-batch_size = 128
-discriminator = Discriminator(img.flatten().size()).to(device)
-generator = Generator(latent_size, img.flatten().size()).to(device)
-dis_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.005)
-gen_optimizer = torch.optim.Adam(generator.parameters(), lr=0.005)
 
-epoch = 100
-for epoch in range(epoch):
+discriminator = Discriminator(len(img.flatten())).to(device)
+discriminator = nn.DataParallel(discriminator, output_device=[0,1])
+generator = Generator(latent_size, len(img.flatten())).to(device)
+generator = nn.DataParallel(generator, output_device=[0,1])
+dis_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0001)
+gen_optimizer = torch.optim.Adam(generator.parameters(), lr=0.0001)
+
+criterion = nn.BCELoss().to(device)
+for epoch in range(total_epoch):
     for i, (image, label) in enumerate(dataloader):
-        real_image = image.view((batch_size, -1)).to(device)
-        z = torch.randn((batch_size, latent_size)).to(device)
+
+        real_image = image.view((image.size(0), -1)).to(device)
+        ones = torch.ones((image.size(0), 1)).to(device)
+        zeros = torch.zeros((image.size(0), 1)).to(device)
+        # train discriminator
+        dis_optimizer.zero_grad()
+        real_output = discriminator(real_image)
+        z = torch.randn((image.size(0), latent_size)).to(device)
         fake_image = generator(z)
+        fake_output = discriminator(fake_image)
         
+        real_loss = criterion(real_output, ones)
+        fake_loss = criterion(fake_output, zeros)
+        discriminator_loss = real_loss + fake_loss
+        discriminator_loss.backward()
+        dis_optimizer.step()
+
+        # train generator
+        gen_optimizer.zero_grad()
+        z = torch.randn((image.size(0), latent_size)).to(device)
+        fake_image = generator(z)
+        fake_output = discriminator(fake_image)
+        generator_loss = criterion(fake_output, ones)
+
+        generator_loss.backward()
+        gen_optimizer.step()
+    if epoch == 0:
+        real_image = real_image.view(real_image.size(0), 1, 28, 28)
+        save_image(real_image[:25], "./result/real.png", nrow=5, normalize=True)
+    if epoch % 10 == 0:
+        fake_image = fake_image.view(fake_image.size(0), 1, 28, 28)
+        save_image(fake_image, f"./result/{epoch}.png", normalize=True)
+        
+    print(f'Epoch {epoch}/{total_epoch} || discriminator loss={discriminator_loss:.4f}  || generator loss={generator_loss:.4f}')
+torch.save(discriminator.state_dict(), "discriminator.ckpt")
+torch.save(generator.state_dict(), "generator.ckpt")
 
 # experiment
 # does order matter? g then d // d then g -> in the paper, d->g
-# how about uniform distribution for noise distribution
+# how about cnn rather than fcnn?
+# learning rate : 0.001 -> loss diverge
